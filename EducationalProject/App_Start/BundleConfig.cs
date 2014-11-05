dc76b21385784d74a6ea7711d5bc1a12d99a9a23
Demetr <dmitri.martynov@gmail.com>
2014-11-05 15:39:44 +0200

    Changing template to justified-nav

--- EducationalProject/App_Start/BundleConfig.cs
+++ EducationalProject/App_Start/BundleConfig.cs
@@ -3,7 +3,7 @@ using System.Web.Optimization;
 
 namespace EducationalProject
 {
-    public class BundleConfig1
+    public class BundleConfig
     {
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
@@ -43,7 +43,8 @@ namespace EducationalProject
 
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/bootstrap-theme.css"));
+                "~/Content/bootstrap-theme.css",
+                "~/Content/justified-nav.css"));
         }
     }
 }
