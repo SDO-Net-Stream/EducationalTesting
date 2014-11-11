491b8882c038cb7cc18b9faca17144e9ee097efa
Demetr <dmitri.martynov@gmail.com>
2014-11-11 16:56:10 +0200

    New EduTest project

---
+++ EduTest/EduTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EduTest.MvcApplication" Language="C#" %>
---
+++ EduTest/EduTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EduTest.Startup))]
+namespace EduTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
