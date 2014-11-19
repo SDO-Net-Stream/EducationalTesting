af3d646444f607f6605282f0d5b194f4523a8c7e
Maryan.Provashynskyy <Maryan.Provashynskyy@eleks.com>
2014-11-19 19:10:00 +0200

    UI for lost password action

---
+++ EduTesting/EduTesting.Web/App/Main/views/account/lostPassword.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    var controllerId = 'app.views.account.lostPassword';
+    angular.module('app').controller(controllerId, [
+        '$scope', 'user', 'abp.services.app.account', 'message', '$state',
+        function ($scope, user, accountService, message, $state) {
+            var vm = this;
+            $scope.user = {};
+            $scope.submit = function () {
+                if ($scope.form.$valid) {
+                    accountService.resetPassword($scope.user).success(function () {
+                        message.success("Check your email");
+                    });
+                }
+                return false;
+            };
+        }
+    ]);
+})();
